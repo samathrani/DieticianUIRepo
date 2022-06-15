f64cb0606a5fea42d4d4118956014c9ce71d58c5
EzhilVanan <ezhilvanan@EzhilVanans-MBP.lan>
2022-06-15 18:27:23 -0500

    Combined the features

--- src/test/java/stepDefinition/MyPatientpage.java
+++ src/test/java/stepDefinition/MyPatientpage.java
@@ -12,30 +12,75 @@ public class MyPatientpage {
 	
 	@Given("User is on any page after login")
 	public void user_is_on_any_page_after_login() {
-	   
+		System.out.println("User is in dietician UI");
 	}
 
 	@When("User clicks on My Patients tab")
 	public void user_clicks_on_my_patients_tab() {
+		System.out.println("click My patients tab");
 	}
 
 	@Then("My Patients tab is selected or highlighted")
 	public void my_patients_tab_is_selected_or_highlighted() {
-	   
-	}
-
-	@Then("It shows breadcrubs as Dietician Software MyPatients")
-	public void it_shows_breadcrubs_as_dietician_software_my_patients() {
-	   
+		System.out.println("Highlight my patients tab");
 	}
 
 	@Then("Page title is displayed as My Patients")
 	public void page_title_is_displayed_as_my_patients() {
-	   
+		System.out.println("Display My patients");
 	}
 
-	@Then("Name, email and phone number filter options and search button are displayed")
-	public void name_email_and_phone_number_filter_options_and_search_button_are_displayed() {
-	   
+	@Then("Name, email, phone number filter options and search button are displayed")
+	public void name_email_phone_number_filter_options_and_search_button_are_displayed() {
+		System.out.println("Display name,email,phone number and search button");
+	}
+	@Given("User is on my patient page")
+	public void user_is_on_my_patient_page() {
+		System.out.println("User is on my patient page");
+	}
+
+	@When("User clicks search on my patient")
+	public void user_clicks_search_on_my_patient() {
+		System.out.println("lick my patient");
+	}
+
+	@Then("cust Id, name, detail, last visit and actions are shown in column")
+	public void cust_id_name_detail_last_visit_and_actions_are_shown_in_column() {
+		System.out.println("Show patient details");
+	}
+
+	@Given("User in on my patient after clicked search")
+	public void user_in_on_my_patient_after_clicked_search() {
+		System.out.println("click search");
+	}
+
+	@When("User clicks on button View Previous Diet Plans")
+	public void user_clicks_on_button_view_previous_diet_plans() {
+		System.out.println("click view previous diet plan");
+	}
+
+	@Then("It redirects user to Generated Diet Plans page")
+	public void it_redirects_user_to_generated_diet_plans_page() {
+		System.out.println("redirects to Generated Diet Plans page");
+	}
+
+	@When("User clicks on button Create New Report\\/Plan")
+	public void user_clicks_on_button_create_new_report_plan() {
+		System.out.println("click Create New Report");
+	}
+
+	@Then("It redirects user to Confirm Health Conditions and Generate a New Diet plan page")
+	public void it_redirects_user_to_confirm_health_conditions_and_generate_a_new_diet_plan_page() {
+		System.out.println("redirects to Confirm Health Conditions");
+	}
+
+	@When("User clicks on button View Previous Test Reports on patient record")
+	public void user_clicks_on_button_view_previous_test_reports_on_patient_record() {
+		System.out.println("click View Previous Test Reports");
+	}
+
+	@Then("It redirects user to View Patient Test Reports page")
+	public void it_redirects_user_to_view_patient_test_reports_page() {
+		System.out.println("redirect to View Patient Test Reports ");
 	}
 }
--- src/test/java/stepDefinition/searchPatients.java
+++ src/test/java/stepDefinition/searchPatients.java
@@ -17,30 +17,80 @@ public class searchPatients {
 
 	@Then("Text inside name, email and phone number filter box should disappear")
 	public void text_inside_name_email_and_phone_number_filter_box_should_disappear() {
+		System.out.println("text in filter box should disappear");
 	   
 	}
 
 	@When("User gets data from data sheet with {string} and {string}")
 	public void user_gets_data_from_data_sheet_with_and_sheet1(String testId,String sheetName) {
-	   
+	   System.out.println("enter name, email ");
 	}
 	@When("User gets data from data sheet with {int} and sheet1")
 	public void user_gets_data_from_data_sheet_with_and_sheet1(Integer int1) {
+		System.out.println("enter phone number");
 	}
 	@Then("Records for <name phrase> are shown")
 	public void records_for_name_phrase_are_shown() {
+		System.out.println("show patient results for given name");
 	  
 	}
 
 	@Then("Records for <email phrase> are shown")
 	public void records_for_email_phrase_are_shown() {
+		System.out.println("show patient results for given email");
 	    
 	}
 
 	@Then("Records for <phone number> are shown")
 	public void records_for_phone_number_are_shown() {
+		System.out.println("show patient results for given phone number");
 	  
 	}
-	
+	@Given("User is on my patients page after logged in")
+	public void user_is_on_my_patients_page_after_logged_in() {
+		System.out.println("user in my patient page");
+	    
+	}
+
+	@When("User types in anything other than number in phone number field")
+	public void user_types_in_anything_other_than_number_in_phone_number_field() {
+		System.out.println("user types invalid data in phone number"); 
+	}
+
+	@Then("It must throw error message and discontinue search action")
+	public void it_must_throw_error_message_and_discontinue_search_action() {
+		System.out.println("Display error message");
+	}
+
+	@When("User types in anything other valid character in email address field")
+	public void user_types_in_anything_other_valid_character_in_email_address_field() {
+		System.out.println("user types invalid data in email");
+	}
+
+	@When("User types in anything other alphabets in name field")
+	public void user_types_in_anything_other_alphabets_in_name_field() {
+		System.out.println("user types invalid data in name");
+	}
+
+	@When("User clicks on search button with all fields empty")
+	public void user_clicks_on_search_button_with_all_fields_empty() {
+		System.out.println("click search button");
+	}
+
+	@Then("Displays all patients for that dietician only")
+	public void displays_all_patients_for_that_dietician_only() {
+		System.out.println("Displays all patients");
+	}
+
+	@When("User clicks on search button with or without all fields empty")
+	public void user_clicks_on_search_button_with_or_without_all_fields_empty() {
+		System.out.println("clicks search button");
+	}
+
+	@Then("It shows columns name like Record Number, Cust ID, Name, Details, Last Visit and Actions")
+	public void it_shows_columns_name_like_record_number_cust_id_name_details_last_visit_and_actions() {
+		System.out.println("show patient details");
+	}
+
 	
 }
