302703ad633cf2d0659dd88458119bda7cffad11
Thanusha Rajan <Thanusha Rajan@192.168.1.10>
2022-06-15 17:48:27 -0500

    Added page classes
    (cherry picked from commit 7f1e66456c7ceed4532fdb2e5a38e8f1eafc50b1)

---
+++ src/main/java/pageClasses/RegisterPage.java
@@ -0,0 +1,15 @@
+package pageClasses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class RegisterPage {
+	// Capture Elements belonging to this page
+
+	public RegisterPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	// Actions that can be performed on
+
+}
